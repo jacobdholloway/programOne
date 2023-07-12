d778d760d001d256358f37c99f190dfec7e03912
Jacob Holloway <cssc1729@edoras.sdsu.edu>
2023-07-12 13:05:18 -0700

    added one folder

---
+++ one/mulproc.c
@@ -0,0 +1,43 @@
+/*
+ This program will fork two child processes running the two programs generated in programming zero in parallel.
+ Hint: You mant need to use fork(), exec() family, wait(), exit(), getpid() and etc   ...
+ 
+ Requirements:
+ 
+
+ 1) Exactly two child processes are created, one to run testspecial program and the other is to run testalphabet program;
+ 2) When a program starts to run, print a message to the output screen showing which process (with PID) is running which program, for example:
+    "CHILD <16741> process is executing testalphabet program!"
+    
+ 3) When a program is done, the process exits and at the same time, a message should be print to the output screen showing which  process (with PID) is done with the program, for example:
+    "CHILD <16741> process has done with testalphabet program !"
+    
+ 4) The messages should match the real execution orders, i.e. when the testspecial program starts/ends, the right message should be print out. So you need to figure out how to get the starting/ending time of each 
+ process.
+   
+   
+ The expected screen print out should be similar as follows:
+ 
+ CHILD <16741> process is executing testalphabet program!
+ CHILD <16742> process is executing testspecial program!
+, -> 745668
+. -> 798072
+... ...
+
+CHILD <16742> process has done with testspecial program !
+a -> 2973036
+b -> 556908
+... ...
+
+CHILD <16741> process has done with testalphabet program !
+ */
+
+
+int main(void) {
+		
+		// fill your code here ... 
+		
+	
+	return 0;
+	
+}
